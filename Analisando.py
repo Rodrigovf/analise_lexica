0fcde0b3ebfd5f9fed6ea36817d7d511019c6b35
rodrigovf <rodrigo.v.figueiredo@live.com>
2018-02-16 11:42:06 -0300

    Inserido numero do identificador

--- Analisando.py
+++ Analisando.py
@@ -7,13 +7,6 @@ class Analise(object):
 
         for linha in arquivo:
             token = linha.split(' ')
-
-            print('#####################################################################\n')
-            print(letra +' == '+ token[1]) 
-            print(palavra +' == '+ token[1])
-            print(token)
-            vazio= ' '
-
             if(letra == token[1]):
                 return [letra, token[0]]
             elif(palavra == token[1]):
@@ -22,45 +15,48 @@ class Analise(object):
                 return [letra, palavra]
         return [None, None]
 
-    def verificaFrase(self, frase, nlinha):
+    def verificaFrase(self, frase, numlinha):
         lista = list(frase)
         data = ''
         linha = ''
-        numLinha = str(nlinha)
+        numLinha = str(numlinha)
         coluna = 0
+        contId=0
 
         for i in range(len(lista)):
             linha = linha + str(lista[i])
             condicao = self.analisaFrase(lista[i],linha)
 
             if(condicao[1] == '1'):
-                data += condicao[0] + ' ---> Palavra Reservada ---> '+ numLinha  +'  ---> '+ str(coluna) +' \n'
+                data += condicao[0] + ' | Palavra Reservada | '+ numLinha  +'  | '+ str(coluna) +' \n'
                 linha = ''
                 coluna = i -1
             elif(condicao[1] == '2'):
                 if(len(linha) == 1):
-                    data += condicao[0] + ' ---> Operador ---> '  + numLinha  +'  ---> '+ str(coluna) +' \n'
+                    data += condicao[0] + ' | Operador | '  + numLinha  +'  | '+ str(coluna) +' \n'
                     linha = ''
                     coluna = i +1
                 else:
                     op = str(condicao[0])
                     s = linha.replace(op,'')
-                    data += s + ' ---> Identificador ---> '  + numLinha  +'  ---> '+ str(coluna) +' \n'
-                    data += condicao[0] + ' ---> Operador ---> '  + numLinha +'  ---> '+ str(coluna + len(s)) +' \n'
+                    data += s + ' | Identificador['+str(contId)+'] | '  + numLinha  +'  | '+ str(coluna) +' \n'
+                    data += condicao[0] + ' | Operador | '  + numLinha +'  | '+ str(coluna + len(s)) +' \n'
                     linha = ''
                     coluna = i +1
+                    contId += 1
             elif(condicao[1] == '3'):
                 if(len(linha) == 1):
-                    data += condicao[0] + ' ---> Delimitador ---> '  + numLinha  +'  ---> '+ str(coluna) +' \n'
+                    data += condicao[0] + ' | Delimitador | '  + numLinha  +'  | '+ str(coluna) +' \n'
                     linha = ''  
                     coluna = i +1
                 else:
                     dl = str(condicao[0])
                     s = linha.replace(dl,'')
-                    data += s + ' ---> Identificador ---> '  + numLinha +'  ---> '+ str(coluna) +' \n'
-                    data += condicao[0] + ' ---> Delimitador ---> '  + numLinha +'  ---> '+ str(coluna + len(s)) +' \n'
+                    data += s + ' | Identificador['+str(contId)+'] | '  + numLinha +'  | '+ str(coluna) +' \n'
+                    data += condicao[0] + ' | Delimitador | '  + numLinha +'  | '+ str(coluna + len(s)) +' \n'
                     linha = ''
                     coluna = i +1
+                    contId += 1
             elif(str(condicao[0]) == ' '):
                 if(len(linha) == 1):
                     linha = ''  
@@ -68,14 +64,15 @@ class Analise(object):
                 else:
                     s = linha.replace(" ",'')
                     print(s)
-                    data += s + ' ---> Identificador ---> '  + numLinha +'  ---> '+ str(coluna) +' \n'
+                    data += s + ' | Identificador['+str(contId)+'] | '  + numLinha +'  | '+ str(coluna) +' \n'
                     linha = ''
                     coluna = i +1
+                    contId += 1
             else:
                 print('haha')
 
         if(len(linha) > 0):
-            data += linha + ' ---> Identificador ---> '  + numLinha +' \n'
+            data += linha + ' | Identificador['+str(contId)+'] | '  + numLinha +' \n'
             linha = ''  
         
         return data
